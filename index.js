98bf55369097ae3b11e89d69733105ae0b987f00
kurwitz3 <kurwitz3@aol.com>
2021-05-14 15:38:59 -0400

    commit tree 1eb4d0a98fa30954f6e5236bf0ca42c0aeb33238

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection,callback) {
+      if(typeof collection === 'object'){
+        let values = Object.values(collection)
+        values.forEach(value => {
+           return callback(value)
+        })
+      }
+      return collection
     },
 
-    map: function() {
+    map: function(collection,callback) {
+      if(typeof collection === 'object'){
+        const values = Object.values(collection)
+         let a = values.map(newValues =>{
+           callback(newValues)
+        })
 
+      }
+      else{
+         let a = collection.map(value => {
+          callback(value)
+        })
+      return a 
+      }
     },
 
     reduce: function() {
